e3a7808559cf9a3ace43cfe98069e41483e83642
Jiang Nane <zero.jy.codeing@gmail.com>
2016-06-09 11:49:06 +0900

    all

---
+++ My first project/My first project/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  My first project
+//
+//  Created by liuyan on 16/6/9.
+//  Copyright (c) 2016年 liuyan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
